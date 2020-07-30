6b9d506862cd4239ba7b34d15091ab3c52888e48
Marjan Tanevski <marjan.tanevski@netent.com>
2020-07-30 23:23:40 +0200

    test

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -9,21 +9,29 @@
         style="width: 100%; max-width:100%"
       >
         <v-row class="fill-height" align-content="center" justify="center">
-          <v-col cols="12" class="display-2 text-center">COMPANY</v-col>
-          <v-col cols="8">
-            <v-progress-linear color="amber accent-4" :value="progressValue" rounded height="6"></v-progress-linear>
+          <v-col cols="12" class="display-2 text-center">
+            <logo />
           </v-col>
-          <v-col class="subtitle-1 text-center" cols="12">{{ progressReason }}</v-col>
+          <v-col cols="12" class="display-2 text-center">
+          <loading-dots />
+          </v-col>
         </v-row>
+        
       </v-container>
     </v-fade-transition>
   </v-app>
 </template>
 
 <script>
+import LoadingDots from "./sub/LoadingDots";
+import Logo from "./sub/Logo";
+
 export default {
   name: "LoadingScreen",
-
+  components: {
+    Logo,
+          LoadingDots
+  },
   data() {
     return {
       progressValue: 0,
@@ -35,7 +43,7 @@ export default {
     progress(stats) {
       this.$data.progressReason = stats.reason;
       this.$data.progressValue = stats.value;
-      if (this.$data.progressValue >= 100) {
+      if (this.$data.progressValue >= 1000) {
         setTimeout(() => {
           this.$data.loading = false;
           this.$emit(events.loadingFinished);
